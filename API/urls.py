f24b8ed1083d9d149ec76d78e7d849fcf5ca0ee8
Hénorie <henorie.soulet@gmail.com>
2024-05-13 08:53:33 +0200

    wouuhou

---
+++ API/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views  # Assurez-vous que vos vues sont correctement importées
+
+urlpatterns = [
+    path('', views.home, name='home'),  # Route pour la page d'accueil
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('graphiques/', views.graphiques, name='graphiques'),
+    path('indicators/', views.indicators, name='indicators'),
+    path('dashboard/', views.dashboard, name='dashboard')
+]
--- API/views.py
+++ API/views.py
@@ -8,6 +8,15 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+def graphiques(request):
+    return render(request, 'graphiques.html')
+
+def indicators(request):
+    return render(request, 'indicators.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
 from django.shortcuts import render
 
 # Create your views here.
