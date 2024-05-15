90a12bd5c009af122d876a12df7f8264ca17f8f5
Vallot <christophe.vallotpro@gmail.com>
2024-05-15 21:31:08 +0200

    show data in csv

--- API/views.py
+++ API/views.py
@@ -1,21 +1,45 @@
+import csv
+import pandas
+from django.http import HttpResponse
+
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'home.html')
+    global data
+
+    # print(request.POST.get("file"))
+    path = 'API/asset/export_rdv_2023-10-01-2023-12-31.csv'
+    data = pandas.read_csv(path, sep=";")
+    return render(request, 'home.html', {'data': data})
+    # return HttpResponse(data.to_html())
+
+    # if 'data' in vars():
+    #     print("OK")
+    #     return render(request, 'home.html', {'data': data})
+    # else:
+    #     return render(request, 'home.html')
+
 
 def about(request):
     return render(request, 'about.html')
 
+
 def contact(request):
     return render(request, 'contact.html')
+
+
 def graphiques(request):
     return render(request, 'graphiques.html')
+
 
 def indicators(request):
     return render(request, 'indicators.html')
 
+
 def dashboard(request):
     return render(request, 'dashboard.html')
+
 
 from django.shortcuts import render
 
