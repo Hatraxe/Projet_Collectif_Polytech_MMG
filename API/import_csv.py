9ad302cad308feb4b5aa7ba6817a0e724cc7039c
Vallot <christophe.vallotpro@gmail.com>
2024-05-28 21:01:58 +0200

    add system to remove some raw in Notes + remove \r et \n from raw data

--- API/import_csv.py
+++ API/import_csv.py
@@ -1,32 +1,46 @@
+import re
 import sqlite3
 
 
 def csv_to_sqlite(data):
-
-    data = clear_csv(data)
+    # data = clear_csv(data)
     database = sqlite3.connect("db.sqlite")
     data.to_sql('csv_data', database, if_exists='replace', index=False)
-    return data
+    # return data
 
-def words_to_remove():
-    filin = open("mot_a_retire.txt", "r")
-    rows_to_remove = filin.readlines()
-    return rows_to_remove
+
+
 
 
 # Remove unexpect data in csv file like rest
 def clear_csv(data):
+    print(data.Notes)
     data = data.fillna("")  # remove "nan"
-    try :
+    data['Notes'] = data['Notes'].apply(lambda x: re.sub(r'[\r\n]', '', x)) # en deux regex ...
+    data['Notes'] = data['Notes'].apply(lambda x: re.sub(r'\s+$', '', x))
+    print(data.Notes)
+    try:
         rows_to_remove = words_to_remove()
     except:
         rows_to_remove = []
+    # print(rows_to_remove)
     save_word_to_remove(rows_to_remove)
     for i in range(len(rows_to_remove)):
         data = data.drop(data[(data.Notes == rows_to_remove[i])].index)
     return data
 
+### file contain a list of word to remove ###
+#Save
 def save_word_to_remove(words):
     filout = open("mot_a_retire.txt", "w")
     for word in words:
-        filout.write(f"{word}\n")
+        if word != "" or word != "\n":
+            filout.write(f"{word}\n")
+
+def words_to_remove():
+    rows_to_remove = []
+    filin = open("mot_a_retire.txt", "r")
+    lines = filin.readlines()
+    for line in lines:
+        rows_to_remove.append(line.replace("\n",""))
+    return rows_to_remove
