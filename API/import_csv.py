de6f41898e48f5602986640d3b08e60caafffc94
Vallot <christophe.vallotpro@gmail.com>
2024-05-28 18:24:56 +0200

    fix problem in import

--- API/import_csv.py
+++ API/import_csv.py
@@ -2,17 +2,31 @@ import sqlite3
 
 
 def csv_to_sqlite(data):
-    data = data.fillna("")  # remove "nan"
-    data.drop_duplicates(keep='last')  # remove duplication (when multiple importations)
+
     data = clear_csv(data)
     database = sqlite3.connect("db.sqlite")
-    data.to_sql('csv_data', database, if_exists='append', index=False)
+    data.to_sql('csv_data', database, if_exists='replace', index=False)
     return data
 
+def words_to_remove():
+    filin = open("mot_a_retire.txt", "r")
+    rows_to_remove = filin.readlines()
+    return rows_to_remove
 
-# Remove unexpect data in csv file like rest (with a filters list)
-def clear_csv(data, filters):
-    for i in range(len(filters)):
-        data = data.drop(data[(data.Notes == filters[i])].index)
+
+# Remove unexpect data in csv file like rest
+def clear_csv(data):
+    data = data.fillna("")  # remove "nan"
+    try :
+        rows_to_remove = words_to_remove()
+    except:
+        rows_to_remove = []
+    save_word_to_remove(rows_to_remove)
+    for i in range(len(rows_to_remove)):
+        data = data.drop(data[(data.Notes == rows_to_remove[i])].index)
     return data
 
+def save_word_to_remove(words):
+    filout = open("mot_a_retire.txt", "w")
+    for word in words:
+        filout.write(f"{word}\n")
